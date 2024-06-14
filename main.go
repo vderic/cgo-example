8157a94803d6471ad900cf2149d7f4d9b235ecf6
vderic <ericlam@vitessedata.com>
2024-06-14 11:35:39 +0000

    go routine

--- main.go
+++ main.go
@@ -13,27 +13,28 @@ package main
 import "C"
 import (
 	"fmt"
+	"sync"
 	"time"
 	"unsafe"
-//	"github.com/vderic/cgo-example/peachpy"
+	// "github.com/vderic/cgo-example/peachpy"
 )
 
 func go_dot_product(x []float32, y []float32, n uint) float64 {
 	result := float64(0)
-	for i := 0 ; i < int(n) ; i++ {
+	for i := 0; i < int(n); i++ {
 		result += float64(x[i] * y[i])
 	}
 	return result
 }
 
 func go_sum(r []float32, x []float32, y []float32, n uint) {
-	for i := 0 ; i < int(n) ; i++ {
+	for i := 0; i < int(n); i++ {
 		r[i] = x[i] * y[i]
 	}
 }
 
 func go_sum_overhead(r []float32, x []float32, y []float32, n uint) {
-	for i := 0 ; i < int(n) ; i++ {
+	for i := 0; i < int(n); i++ {
 		r[i] = multiply(x[i], y[i])
 	}
 }
@@ -43,15 +44,31 @@ func multiply(x float32, y float32) float32 {
 	return x * y
 }
 
+func gor_sum(r []float32, x []float32, y []float32, n uint) {
+	var wg sync.WaitGroup
+	nthread := 2
+	chunkSize := int(n) / nthread
+	for i := 0; i < nthread; i++ {
+		wg.Add(1)
+		go func(start, end int) {
+			defer wg.Done()
+			for j := start; j < end; j++ {
+				r[j] = x[j] * y[j]
+			}
+		}(i*chunkSize, (i+1)*chunkSize)
+	}
+	wg.Wait()
+}
+
 func inc(n int) int {
-	for i := 0 ; i < 1000000000 ; i++ {
+	for i := 0; i < 1000000000; i++ {
 		n += 1
 	}
 	return n
 }
 
 func inc_overhead(n int) int {
-	for i := 0 ; i < 1000000000 ; i++ {
+	for i := 0; i < 1000000000; i++ {
 		n = add(n)
 	}
 	return n
@@ -59,77 +76,82 @@ func inc_overhead(n int) int {
 
 //go:noinline
 func add(n int) int {
-	return n+1
+	return n + 1
 }
-
 
 func main() {
 	n := 100000000
 	x := make([]float32, n)
-        y := make([]float32, len(x))
-        for i := 0; i < len(x); i++ {
-                x[i] = 2.0
-                y[i] = 3.0
-        }
+	y := make([]float32, len(x))
+	r := make([]float32, len(x))
+	for i := 0; i < len(x); i++ {
+		x[i] = 2.0
+		y[i] = 3.0
+	}
 
 	start := time.Now()
 
 	/*
-	diff := time.Since(start)
-	n := 5
-	result := C.avx(C.int(n))
+		diff := time.Since(start)
+		n := 5
+		result := C.avx(C.int(n))
 
-	diff := time.Since(start)
+		diff := time.Since(start)
 
-	fmt.Printf("Factorial of %d is %d\n", n, result)
+		fmt.Printf("Factorial of %d is %d\n", n, result)
 
-	fmt.Printf("time = %d\n", diff)
+		fmt.Printf("time = %d\n", diff)
 
-	start = time.Now()
-	result = C.sse(C.int(n))
-	diff = time.Since(start)
-	fmt.Printf("time = %d\n", diff)
+		start = time.Now()
+		result = C.sse(C.int(n))
+		diff = time.Since(start)
+		fmt.Printf("time = %d\n", diff)
 
-	start = time.Now()
-	result = C.sse2(C.int(n))
-	diff = time.Since(start)
-	fmt.Printf("time = %d\n", diff)
+		start = time.Now()
+		result = C.sse2(C.int(n))
+		diff = time.Since(start)
+		fmt.Printf("time = %d\n", diff)
 
-	n = 1000000000
+		n = 1000000000
 
-	start = time.Now()
-	gccresult := C.gcc_dot_product((*C.float)(unsafe.Pointer(&x[0])), (*C.float)(unsafe.Pointer(&y[0])), C.uint(len(x)))
-	diff = time.Since(start)
-	//fmt.Printf("vectorized dot product: time = %d\n", diff)
-	//fmt.Printf("Vectorize dot product of is %f\n", gccresult)
+		start = time.Now()
+		gccresult := C.gcc_dot_product((*C.float)(unsafe.Pointer(&x[0])), (*C.float)(unsafe.Pointer(&y[0])), C.uint(len(x)))
+		diff = time.Since(start)
+		//fmt.Printf("vectorized dot product: time = %d\n", diff)
+		//fmt.Printf("Vectorize dot product of is %f\n", gccresult)
 
-	start = time.Now()
-	goresult := go_dot_product(x, y, uint(len(x)))
-	diff = time.Since(start)
-	//fmt.Printf("go dot proudct: time = %d\n", diff)
-	//fmt.Printf("Go dot product = %f\n", goresult)
+		start = time.Now()
+		goresult := go_dot_product(x, y, uint(len(x)))
+		diff = time.Since(start)
+		//fmt.Printf("go dot proudct: time = %d\n", diff)
+		//fmt.Printf("Go dot product = %f\n", goresult)
 	*/
 
-	r := make([]float32, len(x))
 	start = time.Now()
-	C.gcc_sum((*C.float)(unsafe.Pointer(&r[0])), (*C.float)(unsafe.Pointer(&x[0])), (*C.float)(unsafe.Pointer(&y[0])),  C.uint(len(x)))
+	gor_sum(r, x, y, uint(len(x)))
+	diff4 := time.Since(start)
+	//fmt.Printf("go routine sum: time = %d\n", diff4)
+
+	start = time.Now()
+	C.gcc_sum((*C.float)(unsafe.Pointer(&r[0])), (*C.float)(unsafe.Pointer(&x[0])), (*C.float)(unsafe.Pointer(&y[0])), C.uint(len(x)))
 	diff1 := time.Since(start)
 	//fmt.Printf("vectorized sum: time = %d\n", diff1)
 
 	start = time.Now()
-	go_sum(r, x, y,  uint(len(x)))
+	go_sum(r, x, y, uint(len(x)))
 	diff2 := time.Since(start)
 	//fmt.Printf("go sum: time = %d\n", diff2)
 
 	start = time.Now()
-	go_sum_overhead(r, x, y,  uint(len(x)))
+	go_sum_overhead(r, x, y, uint(len(x)))
 	diff3 := time.Since(start)
 	//fmt.Printf("go sum overhead: time = %d\n", diff3)
 
 	fmt.Printf("Mutliply function\n")
-	fmt.Printf("       |    GO            |  GO OVERHEAD          |    GCC     \n")
-	fmt.Printf(" Time  |    %d     |      %d       |    %d\n", diff2, diff3, diff1)
-	fmt.Printf("ratio compared to go: (GO/GO) 1 vs (GO_OVERHEAD/GO) %f vs (GCC/GO) %f\n",  float32(diff3)/float32(diff2), float32(diff1)/float32(diff2))
+	fmt.Printf("       |    GO            |  GO OVERHEAD          |    GCC       |  Go Routine\n")
+	fmt.Printf(" Time  |    %d     |      %d       |    %d        |      %d\n", diff2, diff3, diff1, diff4)
+	fmt.Printf("ratio compared to go: (GO/GO) 1 vs (GO_OVERHEAD/GO) %f vs (GCC/GO) %f vs (Go Rountine/Go) %f\n",
+		float32(diff3)/float32(diff2), float32(diff1)/float32(diff2), float32(diff4)/float32(diff2))
 
 	start = time.Now()
 	increment1 := inc(1000)
@@ -156,28 +178,22 @@ func main() {
 	fmt.Printf(" Time  |    %d     |      %d       |    %d\n", diff1, diff2, diff3)
 	//fmt.Printf("  Ratio |   %f      |  %f           |    %f\n", 1.0, float32(diff2)/float32(diff1), float32(diff3)/float32(diff1))
 
-
-	fmt.Printf("ratio compared to go: (GO/GO) 1 vs (GO_OVERHEAD/GO) %f vs (GCC/GO) %f\n",  float32(diff2)/float32(diff1), float32(diff3)/float32(diff1))
+	fmt.Printf("ratio compared to go: (GO/GO) 1 vs (GO_OVERHEAD/GO) %f vs (GCC/GO) %f\n", float32(diff2)/float32(diff1), float32(diff3)/float32(diff1))
 	/*
 
-	for i := 0 ; i < len(x) ; i++ {
-		fmt.Printf("%f ", r[i])
-	}
-	fmt.Printf("\n")
+		for i := 0 ; i < len(x) ; i++ {
+			fmt.Printf("%f ", r[i])
+		}
+		fmt.Printf("\n")
 	*/
-
 
 	/* peachpy is buggy.  DO NOT USE IT */
 	/*
-	start = time.Now()
-	result := peachpy.DotProduct(&x[0], &y[0], uint(len(x)))
-	diff = time.Since(start)
-	fmt.Printf("time = %d\n", diff)
-	fmt.Printf("z = %f\n", result)
+		start = time.Now()
+		result := peachpy.DotProduct(&x[0], &y[0], uint(len(x)))
+		diff = time.Since(start)
+		fmt.Printf("time = %d\n", diff)
+		fmt.Printf("z = %f\n", result)
 	*/
 
-
 }
-
-
-
